a76cb0409ad036515e60b4075d6b796e831f77a2
AnjaliNagare <anjalinagare20@gmail.com>
2022-08-18 09:39:23 +0200

    Part 1

---
+++ server/db.js
@@ -0,0 +1,41 @@
+const spicedPg = require("spiced-pg");
+const bcrypt = require("bcryptjs");
+
+const { DATABASE_USER, DATABASE_PASSWORD } = require("../secrets.json");
+
+const DATABASE_NAME = "social-network";
+
+// connection to the db
+const db = spicedPg(
+    `postgres:${DATABASE_USER}:${DATABASE_PASSWORD}@localhost:5432/${DATABASE_NAME}`
+);
+
+const hash = (password) =>
+    bcrypt.genSalt().then((salt) => bcrypt.hash(password, salt));
+
+function createUser({ first_name, last_name, email, password }) {
+    return hash(password).then((password_hash) => {
+        return db
+            .query(
+                
+                `INSERT INTO users
+                (first_name, last_name, email, password_hash)
+                VALUES ($1, $2, $3, $4)
+                RETURNING *`,
+                [first_name, last_name, email, password_hash]
+            )
+            .then((result) => result.rows[0]);
+    });
+}    
+
+function getUserById(id) { 
+    return db
+        .query("SELECT * FROM users WHERE id = $1", [id])
+        .then((result) => result.rows[0]);
+}
+
+module.exports = {
+    createUser,
+    getUserById,
+};
+
--- server/server.js
+++ server/server.js
@@ -3,9 +3,56 @@ const app = express();
 const compression = require("compression");
 const path = require("path");
 
+const { SESSION_SECRET } = require("../secrets.json");
+const cookieSession = require("cookie-session");
+
 app.use(compression());
 
+app.use(
+    express.urlencoded({
+        extended: false,
+    })
+);
+
+app.use(
+    cookieSession({
+        secret: SESSION_SECRET,
+        maxAge: 1000 * 60 * 60 * 24 * 14, // two weeks of cookie validity
+        sameSite: true,
+    })
+);
+
 app.use(express.static(path.join(__dirname, "..", "client", "public")));
+app.use(express.json());
+
+const { createUser, getUserById } = require("./db");
+
+app.get("/api/users/me", (request, response)=> {
+    if (!request.session.user_id) {
+        response.json("null");
+        return;
+    }
+    getUserById(request.session.user_id).then((result) =>{
+        console.log(result);
+        response.json(result);
+    });
+});
+
+app.post("/api/users", (request, response) => {
+    createUser(request.body)
+        .then(newUser => {
+            console.log(newUser);
+            request.session.user_id = newUser.id;
+            response.json(newUser);
+        }).catch(error => {
+            console.log('POST /api/users', error);
+            if (error.constraint === "user_email_key") {
+                response.status(400).json({ error: "E-mail already in use" });
+                return;
+            }
+            response.status(500).json({ error: 'Something went wrong' });
+        });
+});
 
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "..", "client", "index.html"));
